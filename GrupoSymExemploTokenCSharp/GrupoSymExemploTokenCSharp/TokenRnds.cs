959a391370805cd7354f90e9b693c69e53c101d1
davidsonrf <davidsonrf@gmail.com>
2020-08-01 22:27:52 -0300

    Exemplo C#

---
+++ GrupoSymExemploTokenCSharp/GrupoSymExemploTokenCSharp/TokenRnds.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace GrupoSymExemploTokenCSharp
+{
+	public class TokenRnds
+	{
+		[JsonPropertyName("access_token")]
+		public string AccessToken { get; set; }
+		[JsonPropertyName("scope")]
+		public string Scope { get; set; }
+		[JsonPropertyName("token_type")]
+		public string TokenType { get; set; }
+		[JsonPropertyName("expires_in")]
+		public long ExpiresIn { get; set; }
+	}
+}
